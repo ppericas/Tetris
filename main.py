ecdc338fe929c4511de678458930147a80fb2ec5
ppericas <peppericasserra02@gmail.com>
2024-03-19 14:34:42 +0100

    upload assets.py, tetris.py, tetromino.py

--- main.py
+++ main.py
@@ -1,40 +1,53 @@
 # matrix of 10 squares of 20 px by 20 of height (+ walls)
 # ---------------------------------------------------------------------------------------------------------------------
 # imports
-import pygame, os, random
-
+import pygame
+from icecream import ic
 pygame.init()
 
 # ---------------------------------------------------------------------------------------------------------------------
 # constants
 ## window
-WIDTH, HEIGHT = 250, 500
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+WIDTH: int= 250
+HEIGHT: int = 500
+WIN: tuple[int] = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Tetris')
 
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
+BLACK: tuple[int] = (0, 0, 0)
+WHITE: tuple[int] = (255, 255, 255)
+LIGHT_GREY: tuple[int] = (210, 210, 210)
+DARK_GREY: tuple[int] = (80, 80, 80)
 
 ## sizes
-SQUARE_SIZE = 25
+SQUARE_SIZE: int = 23
 
 ## others
-FPS = 60
+FPS: int = 60
 
 # ---------------------------------------------------------------------------------------------------------------------
 # class & functions
-def draw_matrix():
-    for i in range (9):
-        for j in range (19):
-            pygame.draw.rect(WIN, WHITE, pygame.Rect(i * SQUARE_SIZE, j * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+def output_to_file(text:str) -> None:
+    with open('debug_log.txt', 'a') as f:
+        f.write(f'{text}\n')
 
 
-def draw_window():
+ic.configureOutput(prefix='Debug| ', outputFunction = output_to_file, includeContext=True)
+
+
+def draw_matrix() -> None:
+    WIN.fill(LIGHT_GREY)
+    for i in range(10):
+        for j in range(20):
+            pygame.draw.rect(WIN, WHITE, pygame.Rect(i * SQUARE_SIZE + i * 2, j * SQUARE_SIZE + j * 2, SQUARE_SIZE, SQUARE_SIZE))
+
+
+def draw_window() -> None:
     draw_matrix()
     pygame.display.update()
 
+
 # main loop
-def main():
+def main() -> None:
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -47,4 +60,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()       
+    main()
---
+++ tetromino.py
@@ -0,0 +1,17 @@
+import random
+from assets import tetrominos, shapes, colors
+
+class Tetromino:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+        self.type = random.choice(tetrominos)
+        self.shape = shapes[self.type]
+        self.color = colors[self.type]
+        self.rotation = 0
+
+    def image(self):
+        return self.shape[self.rotation]
+
+    def rotate(self):
+        self.rotation = (self.rotation + 1) % len(self.shape)
